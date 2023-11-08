b37b1e5bdc06ac1d361ed50227eea7377e43857d
PsychotherapyCentral <psychotherapy.first@gmail.com>
2023-11-08 08:16:27 +0530

    Login Pages Added

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,41 +1,48 @@
-import React, { useState } from 'react'
-{/*
-import user_icon from "../images/person.png"
-import email_icon from 'public/images/email.png'
-import password_icon from 'public/images/password.png'
-*/}
+import React, { useState } from 'react';
+import '../assets/stylesheets/login.css';
+import { Navigate, useNavigate } from 'react-router-dom';
+import user_icon from '../assets/icons/person.png';
+import email_icon from '../assets/icons/email.png';
+import password_icon from '../assets/icons/password.png';
+import back from '../assets/icons/back_button.png';
+
 const LoginSignup = () => {
-
+    const navigate = useNavigate();
     const [action,setAction] = useState("Login");
+    const goBack = () => {
+        navigate('/');
+      };    
   return (
-    <div className ='container'>
-        <div className = 'header'>
-            <div className="text">{action}</div>
-            <div className="underline"></div>
-        </div>
-        <div className="inputs">
-            {action==="Login"?<div></div>: <div className="input">
-                {/*<img src={user_icon} alt="" />*/}
-                <input type="text" placeholder='Name'/>
-            </div>}
-           
-            <div className="input">
-                {/*<img src={email_icon} alt="" />*/}
-                <input type="email" placeholder='Email Id'/>
-            </div>
-            <div className="input">
-                {/*<img src={password_icon} alt="" />*/}
-                <input type="password" placeholder='Password'/>
-            </div>
-        </div>
-            {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
-        
-        <div className="submit-container">
-            <div className={action==="Login"?"Submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-            <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
+    <div className='container'>
+        <button onClick={goBack} className="back-button">
+          <img className='back-button-icon' src={back}></img>
+      </button>
+              <div className='header'>
+                  <div className="text">{action}</div>
+                  <div className="underline"></div>
+              </div>
+              <div className="inputs">
+                  {action === "Login" ? <div></div> : <div className="input">
+                      <img src={user_icon} alt="" />
+                      <input type="text" placeholder='Name' />
+                  </div>}
 
-        </div>
-    </div>
+                  <div className="input">
+                      <img src={email_icon} alt="" />
+                      <input type="email" placeholder='Email Id' />
+                  </div>
+                  <div className="input">
+                      <img src={password_icon} alt="" />
+                      <input type="password" placeholder='Password' />
+                  </div>
+              </div>
+              {action === "Sign Up" ? <div></div> : <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+
+              <div className="submit-container">
+                  <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login"); } }>Login</div>
+
+              </div>
+          </div>
   )
 }
 
